5ab569fdeb8cc62d8cb681f828daec95129d9bde
Niema Moshiri <niemamoshiri@gmail.com>
2025-10-01 18:38:15 -0700

    added permissions output

--- merge_csv.py
+++ merge_csv.py
@@ -6,6 +6,7 @@ Merge HIV-TRACE CSVs from multiple jurisdictions
 # imports
 from csv import reader, writer
 from gzip import open as gopen
+from json import dump as jdump
 from os.path import isfile
 from sys import argv, stderr
 import argparse
@@ -15,10 +16,25 @@ HIVTRACE_MERGE_CSV_VERSION = '0.0.1'
 UNIQUE_ID_FIELDS = ['ehars_uid', 'SequenceID_predq', 'predq_clean_seq'] # fields to uniquely ID a sequence
 HEADER = None # will be overwritten by first loaded CSV
 
+# open a file for reading/writing
+def open_file(fn, mode='rt'):
+    if fn == 'stdin':
+        from sys import stdin as f
+    elif fn == 'stdout':
+        from sys import stdout as f
+    elif fn == 'stderr':
+        from sys import stderr as f
+    elif fn.strip().lower().endswith('.gz'):
+        f = gopen(fn, mode=mode)
+    else:
+        f = open(fn, mode=mode)
+    return f
+
 # parse user args
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-o', '--output_csv', required=False, type=str, default='stdout', help="Output Merged CSV")
+    parser.add_argument('-oc', '--output_csv', required=True, type=str, help="Output Merged CSV")
+    parser.add_argument('-op', '--output_permissions', required=True, type=str, help="Output Permissons JSON")
     parser.add_argument('csv', nargs='+', type=str, help="Input CSV")
     args = parser.parse_args()
     args.csv = set(args.csv)
@@ -27,39 +43,41 @@ def parse_args():
     for fn in args.csv:
         if not isfile(fn):
             raise ValueError("File not found: %s" % fn)
-    if isfile(args.output_csv) and fn != 'stdout':
-        raise ValueError("Output file exists: %s" % fn)
+    for fn in [args.output_csv, args.output_permissions]:
+        if isfile(fn):
+            raise ValueError("Output file exists: %s" % fn)
     return args
 
 # load sequence data from CSV
-def load_csv(fn):
-    if fn.strip().lower().endswith('.gz'):
-        f = gopen(fn, 'rt')
-    else:
-        f = open(fn, 'rt')
+def load_csv(csv_fn):
     data = dict() # same structure as `data` in `main()`
-    for row_num, row in enumerate(reader(f)):
-        if row_num == 0: # header
-            header = [s.strip() for s in row]
-            name2ind = {name:ind for ind, name in enumerate(header)}
-            global HEADER
-            if HEADER is None:
-                HEADER = header
-        else:
-            unique_seq_ID = tuple(row[name2ind[name]] for name in UNIQUE_ID_FIELDS)
-            if unique_seq_ID in data:
-                pass # TODO handle duplicate sequences in single jurisdiction's CSV (currently ignore all but first)
+    with open_file(csv_fn, 'rt') as f:
+        for row_num, row in enumerate(reader(f)):
+            if row_num == 0: # header
+                header = [s.strip() for s in row]
+                name2ind = {name:ind for ind, name in enumerate(header)}
+                global HEADER
+                if HEADER is None:
+                    HEADER = header
             else:
-                data[unique_seq_ID] = {header[ind]:val for ind, val in enumerate(row)}
-    f.close()
+                unique_seq_ID = tuple(row[name2ind[name]] for name in UNIQUE_ID_FIELDS)
+                if unique_seq_ID in data:
+                    pass # TODO handle duplicate sequences in single jurisdiction's CSV (currently ignore all but first)
+                else:
+                    data[unique_seq_ID] = {header[ind]:val for ind, val in enumerate(row)}
     return data
 
 # main script logic
 def main():
+    # set things up
     args = parse_args()
     data = dict() # data[unique_seq_ID] = dict mapping column headers to values
-    for fn in args.csv:
-        curr_data = load_csv(fn)
+    access = dict() # access[csv_fn] = set of unique_seq_ID tuples this jurisdiction has access to
+
+    # load data from CSVs
+    for csv_fn in args.csv:
+        curr_data = load_csv(csv_fn)
+        access[csv_fn] = list(curr_data.keys())
         for unique_seq_ID, unique_seq_vals in curr_data.items():
             if unique_seq_ID in data:
                 existing_vals = data[unique_seq_ID]
@@ -67,15 +85,17 @@ def main():
                     existing_vals['hiv_aids_dx_dt'] = unique_seq_vals['hiv_aids_dx_dt']
             else:
                 data[unique_seq_ID] = unique_seq_vals
-    if args.output_csv.strip().lower().endswith('.gz'):
-        f = gopen(args.output_csv, 'wt')
-    else:
-        f = open(args.output_csv, 'wt')
-    csv_writer = writer(f)
-    csv_writer.writerow(HEADER)
-    for unique_seq_vals in data.values():
-        csv_writer.writerow([unique_seq_vals[name] for name in HEADER])
-    f.close()
+
+    # write output merged CSV
+    with open_file(args.output_csv, 'wt') as f:
+        csv_writer = writer(f)
+        csv_writer.writerow(HEADER)
+        for unique_seq_vals in data.values():
+            csv_writer.writerow([unique_seq_vals[name] for name in HEADER])
+
+    # write output permissions JSON
+    with open_file(args.output_permissions, 'wt') as f:
+        jdump(access, f)
 
 # run script
 if __name__ == "__main__":
